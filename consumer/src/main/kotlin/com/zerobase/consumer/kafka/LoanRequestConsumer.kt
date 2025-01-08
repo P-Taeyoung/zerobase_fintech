71bfbc92bc187ab7178d6314b393ed5b03c44eb4
taeyoung <pty10510@gmail.com>
2025-01-08 14:26:08 +0900

    01_CB사 모듈 개발 및 연동

--- consumer/src/main/kotlin/com/zerobase/consumer/kafka/LoanRequestConsumer.kt
+++ consumer/src/main/kotlin/com/zerobase/consumer/kafka/LoanRequestConsumer.kt
@@ -1,16 +1,21 @@
 package com.zerobase.com.zerobase.consumer.kafka
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.zerobase.com.zerobase.consumer.service.LoanRequestService
 import com.zerobase.com.zerobase.kafka.dto.LoanRequestDto
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
 class LoanRequestConsumer(
-    private val objectMapper: ObjectMapper
-) {
+    private val objectMapper: ObjectMapper,
+    private val loanRequestService: LoanRequestService,
+
+    ) {
     @KafkaListener(topics = ["loan_request"], groupId = "fintech")
     fun loanRequestTopicConsumer(message: String) {
         val loanRequestKafkaDto = objectMapper.readValue(message, LoanRequestDto::class.java)
+
+        loanRequestService.loanRequest(loanRequestKafkaDto)
     }
 }
--- consumer/src/main/kotlin/com/zerobase/consumer/service/LoanRequestService.kt
+++ consumer/src/main/kotlin/com/zerobase/consumer/service/LoanRequestService.kt
@@ -1,19 +1,32 @@
 package com.zerobase.com.zerobase.consumer.service
 
+import com.zerobase.com.zerobase.consumer.dto.ReviewResponseDto
+import com.zerobase.com.zerobase.kafka.dto.LoanRequestDto
 import com.zerobase.domain.domain.LoanReview
 import com.zerobase.domain.repository.LoanReviewRepository
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.stereotype.Service
+import java.time.Duration
 
 @Service
 class LoanRequestService(
     private val loanReviewRepository: LoanReviewRepository
 ) {
-    fun loanRequest() {
-        // TODO : CB Component 로 요청보내기
+    companion object {
+        const val cssUrl = "http://localhost:8081/css/api/v1/request"
+    }
+    fun loanRequest(loanRequestDto: LoanRequestDto) {
+        val reviewResult = loanRequestToCb(loanRequestDto)
+        saveLoanReviewData(reviewResult.toLoanReviewEntity())
     }
 
-    fun loanRequestToCb() {
+    fun loanRequestToCb(loanRequestDto: LoanRequestDto): ReviewResponseDto {
+        val restTemplate = RestTemplateBuilder()
+            .setConnectTimeout(Duration.ofMillis(1000))
+            .setReadTimeout(Duration.ofMillis(1000))
+        .build()
 
+        return restTemplate.postForEntity(cssUrl, loanRequestDto, ReviewResponseDto::class.java).body!!
     }
 
     fun saveLoanReviewData(loanReview: LoanReview) = loanReviewRepository.save(loanReview)
